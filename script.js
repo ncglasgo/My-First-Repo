af8c42c369f78ad041c8e19824447a74e4779e71
ncglasgo <71346534+ncglasgo@users.noreply.github.com>
2021-02-09 17:16:45 -0500

    Added Files

---
+++ script.js
@@ -0,0 +1,3 @@
+var name= prompt ("what is your name?");
+var color= prompt ("what is your favorite color?")
+document.body.querySelector(".writer").innerHTML=name+" is my name! "+color+" is my favorite color!";
